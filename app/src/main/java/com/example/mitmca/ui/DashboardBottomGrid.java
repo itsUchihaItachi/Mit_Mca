15ffe22d5c3467edb84f619d63b76c0a6a90af86
melroy27 <42931694+melroy27@users.noreply.github.com>
2019-10-20 13:34:26 +0530

    Dashboard Grid Repo Made

---
+++ app/src/main/java/com/example/mitmca/ui/DashboardBottomGrid.java
@@ -0,0 +1,16 @@
+package com.example.mitmca.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mitmca.R;
+
+public class DashboardBottomGrid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_bottom_grid);
+    }
+}
