cb2e33179d72dc426ec75f608f026afa1f2e1bce
melroy27 <42931694+melroy27@users.noreply.github.com>
2019-10-21 20:36:28 +0300

    Trying

--- app/src/main/java/com/example/mitmca/ui/DashboardBottomGrid.java
+++ app/src/main/java/com/example/mitmca/ui/DashboardBottomGrid.java
@@ -3,14 +3,24 @@ package com.example.mitmca.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.mitmca.R;
+
+import static com.example.mitmca.R.*;
 
 public class DashboardBottomGrid extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard_bottom_grid);
+        setContentView(layout.activity_dashboard_bottom_grid);
+        String[] myItems = {"Html/css","something","something"};
+        ArrayAdapter<String> adapter =new ArrayAdapter<String>(this, R.layout.list_layout,myItems);
+        ListView myFirstListView = (ListView) findViewById(id.ListView);
+    myFirstListView.setAdapter(adapter);
+
+
     }
 }
