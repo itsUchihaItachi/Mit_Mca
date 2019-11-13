cbea1848b4aa7d0ce25e5d4bffafc7b1ceb70017
itsUchihaItachi <46760410+itsUchihaItachi@users.noreply.github.com>
2019-11-13 16:09:14 +0530

    Map files included

--- app/src/main/java/com/example/mitmca/MapsActivity.java
+++ app/src/main/java/com/example/mitmca/MapsActivity.java
@@ -1,9 +1,10 @@
-package com.example.mapapp;
+package com.example.mitmca;
 
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
+import com.example.mitmca.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
