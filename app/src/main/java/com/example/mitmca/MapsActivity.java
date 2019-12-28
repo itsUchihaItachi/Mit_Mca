c49da391124ab5f3c453d949ae1bf5f078725ce2
Ravi Agrawal <raviagrawal1012@gmail.com>
2019-12-28 10:08:38 +0530

    maps working

--- app/src/main/java/com/example/mitmca/MapsActivity.java
+++ app/src/main/java/com/example/mitmca/MapsActivity.java
@@ -4,6 +4,7 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.example.mitmca.R;
@@ -13,6 +14,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -49,10 +51,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
         float zoomLevel = 18.0f; //This goes up to 21
         // Add a marker in MIT and move the camera
-        LatLng mit = new LatLng(18.5178, 73.8149);
-        mMap.addMarker(new MarkerOptions().position(mit).title("Marker in MIT"));
+        LatLng mit = new LatLng(18.515983, 73.8129008);
+        final Marker m = mMap.addMarker(new MarkerOptions().position(mit).title("Marker in MIT"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(mit));
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mit, zoomLevel));
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                if (marker.equals(m)) {
+                    openMapsOnCLick();
+                }
+                return true;
+            }
+        });
+    }
+        private void openMapsOnCLick() {
+//        Uri gmmIntentUri = Uri.parse("geo:18.515983,73.8129008");
+//        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+//        mapIntent.setPackage("com.google.android.apps.maps");
+//        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+//            startActivity(mapIntent);
+        Uri gmmIntentUri = Uri.parse("google.navigation:q=MIT+College,+Pune+Maharashtra");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
     }
 }
